578316a484f83f30f8bd0b458a1afcdc917ad1cd
crazycloud <151503324@qq.com>
2025-08-17 16:20:20 +0800

    Update main.go

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -2,7 +2,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/crazycloudcc/btcapis"
@@ -13,6 +15,9 @@ func main() {
 		btcapis.WithBitcoindRPC(os.Getenv("BITCOIND_URL"), os.Getenv("BITCOIND_USER"), os.Getenv("BITCOIND_PASS")),
 		btcapis.WithMempoolSpace("https://mempool.space"),
 	)
-	tx, err := c.GetTransaction(ctx, "your-txid-here")
+	tx, err := c.GetTransaction(context.Background(), "your-txid-here")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(tx.TxID, len(tx.Vin), len(tx.Vout), tx.Vsize)
 }
