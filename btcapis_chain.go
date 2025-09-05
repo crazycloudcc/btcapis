2e99d4c4869f6e05e18657254e76973cb0d9574f
crazycloud <151503324@qq.com>
2025-09-05 15:26:20 +0800

    bakup;

--- btcapis_chain.go
+++ btcapis_chain.go
@@ -2,8 +2,6 @@ package btcapis
 
 import (
 	"context"
-
-	"github.com/crazycloudcc/btcapis/types"
 )
 
 // EstimateFeeRate 估计手续费.
@@ -11,32 +9,32 @@ func (c *Client) EstimateFeeRate(ctx context.Context, targetBlocks int) (float64
 	return c.chainClient.EstimateFeeRate(ctx, targetBlocks)
 }
 
-// 查询 UTXO
-func (c *Client) GetUTXO(ctx context.Context, hash [32]byte, index uint32) ([]byte, int64, error) {
-	return c.chainClient.GetUTXO(ctx, hash, index)
-}
+// // 查询 UTXO
+// func (c *Client) GetUTXO(ctx context.Context, hash [32]byte, index uint32) ([]byte, int64, error) {
+// 	return c.chainClient.GetUTXO(ctx, hash, index)
+// }
 
-// 获取节点区块数量
-func (c *Client) GetBlockCount(ctx context.Context) (int, error) {
-	return c.chainClient.GetBlockCount(ctx)
-}
+// // 获取节点区块数量
+// func (c *Client) GetBlockCount(ctx context.Context) (int, error) {
+// 	return c.chainClient.GetBlockCount(ctx)
+// }
 
-// 获取最新区块的hash
-func (c *Client) GetBestBlockHash(ctx context.Context) (string, error) {
-	return c.chainClient.GetBestBlockHash(ctx)
-}
+// // 获取最新区块的hash
+// func (c *Client) GetBestBlockHash(ctx context.Context) (string, error) {
+// 	return c.chainClient.GetBestBlockHash(ctx)
+// }
 
-// 使用区块高度 查询区块哈希
-func (c *Client) GetBlockHash(ctx context.Context, height int64) (string, error) {
-	return c.chainClient.GetBlockHash(ctx, height)
-}
+// // 使用区块高度 查询区块哈希
+// func (c *Client) GetBlockHash(ctx context.Context, height int64) (string, error) {
+// 	return c.chainClient.GetBlockHash(ctx, height)
+// }
 
-// 使用区块block hash 查询区块头
-func (c *Client) GetBlockHeader(ctx context.Context, hash string) (*types.ChainBlock, error) {
-	return c.chainClient.GetBlockHeader(ctx, hash)
-}
+// // 使用区块block hash 查询区块头
+// func (c *Client) GetBlockHeader(ctx context.Context, hash string) (*types.ChainBlock, error) {
+// 	return c.chainClient.GetBlockHeader(ctx, hash)
+// }
 
-// 使用区块block hash 查询区块
-func (c *Client) GetBlock(ctx context.Context, hash string) (*types.ChainBlock, error) {
-	return c.chainClient.GetBlock(ctx, hash)
-}
+// // 使用区块block hash 查询区块
+// func (c *Client) GetBlock(ctx context.Context, hash string) (*types.ChainBlock, error) {
+// 	return c.chainClient.GetBlock(ctx, hash)
+// }
--- internal/chain/apis.go
+++ internal/chain/apis.go
@@ -2,8 +2,6 @@ package chain
 
 import (
 	"context"
-
-	"github.com/crazycloudcc/btcapis/types"
 )
 
 func (c *Client) EstimateFeeRate(ctx context.Context, targetBlocks int) (float64, float64, error) {
@@ -27,77 +25,77 @@ func (c *Client) EstimateFeeRate(ctx context.Context, targetBlocks int) (float64
 	return fee1, fee2, nil
 }
 
-// 查询 UTXO
-func (c *Client) GetUTXO(ctx context.Context, hash [32]byte, index uint32) ([]byte, int64, error) {
-	return c.bitcoindrpcClient.ChainGetUTXO(ctx, hash, index)
-}
+// // 查询 UTXO
+// func (c *Client) GetUTXO(ctx context.Context, hash [32]byte, index uint32) ([]byte, int64, error) {
+// 	return c.bitcoindrpcClient.ChainGetUTXO(ctx, hash, index)
+// }
 
-// 获取节点区块数量
-func (c *Client) GetBlockCount(ctx context.Context) (int, error) {
-	return c.bitcoindrpcClient.ChainGetBlockCount(ctx)
-}
+// // 获取节点区块数量
+// func (c *Client) GetBlockCount(ctx context.Context) (int, error) {
+// 	return c.bitcoindrpcClient.ChainGetBlockCount(ctx)
+// }
 
-// 获取最新区块的hash
-func (c *Client) GetBestBlockHash(ctx context.Context) (string, error) {
-	return c.bitcoindrpcClient.ChainGetBestBlockHash(ctx)
-}
+// // 获取最新区块的hash
+// func (c *Client) GetBestBlockHash(ctx context.Context) (string, error) {
+// 	return c.bitcoindrpcClient.ChainGetBestBlockHash(ctx)
+// }
 
-// 使用区块高度 查询区块哈希
-func (c *Client) GetBlockHash(ctx context.Context, height int64) (string, error) {
-	return c.bitcoindrpcClient.ChainGetBlockHash(ctx, height)
-}
+// // 使用区块高度 查询区块哈希
+// func (c *Client) GetBlockHash(ctx context.Context, height int64) (string, error) {
+// 	return c.bitcoindrpcClient.ChainGetBlockHash(ctx, height)
+// }
 
-// 使用区块block hash 查询区块头
-func (c *Client) GetBlockHeader(ctx context.Context, hash string) (*types.ChainBlock, error) {
-	blk, err := c.bitcoindrpcClient.ChainGetBlockHeader(ctx, hash)
-	if err != nil {
-		return nil, err
-	}
+// // 使用区块block hash 查询区块头
+// func (c *Client) GetBlockHeader(ctx context.Context, hash string) (*types.ChainBlock, error) {
+// 	blk, err := c.bitcoindrpcClient.ChainGetBlockHeader(ctx, hash)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return &types.ChainBlock{
-		Hash:              blk.Hash,
-		Confirmations:     blk.Confirmations,
-		Height:            blk.Height,
-		Version:           blk.Version,
-		VersionHex:        blk.VersionHex,
-		MerkleRoot:        blk.MerkleRoot,
-		Time:              blk.Time,
-		MedianTime:        blk.MedianTime,
-		Nonce:             blk.Nonce,
-		Bits:              blk.Bits,
-		Difficulty:        blk.Difficulty,
-		Chainwork:         blk.Chainwork,
-		NTx:               blk.NTx,
-		PreviousBlockHash: blk.PreviousBlockHash,
-		NextBlockHash:     blk.NextBlockHash,
-	}, nil
-}
+// 	return &types.ChainBlock{
+// 		Hash:              blk.Hash,
+// 		Confirmations:     blk.Confirmations,
+// 		Height:            blk.Height,
+// 		Version:           blk.Version,
+// 		VersionHex:        blk.VersionHex,
+// 		MerkleRoot:        blk.MerkleRoot,
+// 		Time:              blk.Time,
+// 		MedianTime:        blk.MedianTime,
+// 		Nonce:             blk.Nonce,
+// 		Bits:              blk.Bits,
+// 		Difficulty:        blk.Difficulty,
+// 		Chainwork:         blk.Chainwork,
+// 		NTx:               blk.NTx,
+// 		PreviousBlockHash: blk.PreviousBlockHash,
+// 		NextBlockHash:     blk.NextBlockHash,
+// 	}, nil
+// }
 
-// 使用区块block hash 查询区块
-func (c *Client) GetBlock(ctx context.Context, hash string) (*types.ChainBlock, error) {
-	blk, err := c.bitcoindrpcClient.ChainGetBlock(ctx, hash)
-	if err != nil {
-		return nil, err
-	}
-	return &types.ChainBlock{
-		Hash:              blk.Hash,
-		Confirmations:     blk.Confirmations,
-		Height:            blk.Height,
-		Version:           blk.Version,
-		VersionHex:        blk.VersionHex,
-		MerkleRoot:        blk.MerkleRoot,
-		Time:              blk.Time,
-		MedianTime:        blk.MedianTime,
-		Nonce:             blk.Nonce,
-		Bits:              blk.Bits,
-		Difficulty:        blk.Difficulty,
-		Chainwork:         blk.Chainwork,
-		NTx:               blk.NTx,
-		PreviousBlockHash: blk.PreviousBlockHash,
-		NextBlockHash:     blk.NextBlockHash,
-		StrippedSize:      blk.StrippedSize,
-		Size:              blk.Size,
-		Weight:            blk.Weight,
-		Tx:                blk.Tx,
-	}, nil
-}
+// // 使用区块block hash 查询区块
+// func (c *Client) GetBlock(ctx context.Context, hash string) (*types.ChainBlock, error) {
+// 	blk, err := c.bitcoindrpcClient.ChainGetBlock(ctx, hash)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &types.ChainBlock{
+// 		Hash:              blk.Hash,
+// 		Confirmations:     blk.Confirmations,
+// 		Height:            blk.Height,
+// 		Version:           blk.Version,
+// 		VersionHex:        blk.VersionHex,
+// 		MerkleRoot:        blk.MerkleRoot,
+// 		Time:              blk.Time,
+// 		MedianTime:        blk.MedianTime,
+// 		Nonce:             blk.Nonce,
+// 		Bits:              blk.Bits,
+// 		Difficulty:        blk.Difficulty,
+// 		Chainwork:         blk.Chainwork,
+// 		NTx:               blk.NTx,
+// 		PreviousBlockHash: blk.PreviousBlockHash,
+// 		NextBlockHash:     blk.NextBlockHash,
+// 		StrippedSize:      blk.StrippedSize,
+// 		Size:              blk.Size,
+// 		Weight:            blk.Weight,
+// 		Tx:                blk.Tx,
+// 	}, nil
+// }
